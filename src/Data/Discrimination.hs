d69d950f52c8e853a5ee969b0c77737ff383d674
Edward Kmett <ekmett@gmail.com>
2015-02-21 16:39:25 -0500

    shuffle some of the re-exports from the top level module

--- src/Data/Discrimination.hs
+++ src/Data/Discrimination.hs
@@ -5,20 +5,24 @@ module Data.Discrimination
   -- * Data.Discrimination.Join
   , Join(..)
   , inner
-  -- * Portions of Data.Discrimination.Class
+  , outer
+  , leftOuter
+  , rightOuter
+  -- * Data.Discrimination.Class
   , Disorderable
   , Disorder(..)
   , Disorder1(..)
   , Orderable
   , Order(..)
   , Order1(..)
+  -- * Data.Discrimination.Table
+  , Table(..)
   -- * Other Modules
   , module Data.Discrimination.Combinators
-  , module Data.Discrimination.Table
   ) where
 
 import Data.Discrimination.Class
 import Data.Discrimination.Combinators
-import Data.Discrimination.Join
 import Data.Discrimination.Table
+import Data.Discrimination.Join
 import Data.Discrimination.Type
