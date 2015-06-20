77972ab78fe5fbf3f844a9481a8b5ca50b86df01
Edward Kmett <ekmett@gmail.com>
2015-06-20 17:03:52 -0400

    lazy ivar model compiles

--- wip/IVar.hs
+++ wip/IVar.hs
@@ -20,71 +20,70 @@ meq :: forall a b. MVar a -> MVar b -> Maybe (a :~: b)
 meq a b = case unsafeCoerce Refl :: a :~: b of
   Refl | a == b -> Just Refl
        | otherwise -> Nothing
-  
-data K a where 
-  Pure      :: a -> K a
-  Fulfilled :: MVar x -> IO (K a) -> K a
 
-instance Functor K where
+data K s a where
+  Pure      :: a -> K s a
+  Fulfilled :: MVar x -> IO (K s a) -> K s a
+
+instance Functor (K s) where
   fmap f (Pure a) = Pure (f a)
   fmap f (Fulfilled m k) = Fulfilled m (fmap (fmap f) k)
 
-data IVar a where
-  IVar :: MVar (IO (K x)) -> MVar a -> a -> IVar a
+data IVar s a where
+  IVar :: MVar (IO (K s x)) -> MVar a -> a -> IVar s a
 
-demand :: IVar a -> a
+demand :: IVar s a -> a
 demand (IVar _ _ a) = a
 
-newtype I a = I { runI :: forall x. MVar (IO (K x)) -> IO (K a) }
+newtype I s a = I { unI :: forall x. MVar (IO (K s x)) -> IO (K s a) }
 
-fulfill :: IVar a -> a -> I ()
+fulfill :: IVar s a -> a -> I s ()
 fulfill (IVar _ v _) a = I $ \ _ -> do
   putMVar v a
   return $ Fulfilled v $ return (Pure ())
 
-newIVar :: I (IVar a)
+newIVar :: I s (IVar s a)
 newIVar = I $ \mv -> do
   v <- newEmptyMVar
-  return $ Pure $ IVar mv v $ drive mv v where
+  return $ Pure $ IVar mv v (drive mv v)
 
-    drive :: MVar (IO (K x)) -> MVar a -> a
-    drive mv v = unsafePerformIO $ tryTakeMVar v >>= \case
-      Just a -> return a
-      Nothing -> do
-        k <- takeMVar mv
-        k' <- pump k v
-        putMVar mv k'
-        takeMVar v
-    {-# NOINLINE drive #-}
-
-    pump :: IO (K x) -> MVar a -> IO (IO (K x))
+drive :: MVar (IO (K s x)) -> MVar a -> a
+drive = \mv v -> unsafePerformIO $ tryTakeMVar v >>= \case
+  Just a -> return a
+  Nothing -> do
+    k <- takeMVar mv
+    k' <- pump k v
+    putMVar mv k'
+    takeMVar v
+  where
+    pump :: IO (K s x) -> MVar a -> IO (IO (K s x))
     pump m v = m >>= \case
       Pure a -> error "pump: unfulfilled promise"
       Fulfilled u k -> case meq u v of
         Just Refl -> return k
         Nothing   -> pump k v
-  
-instance Functor I where
+{-# NOINLINE drive #-}
+
+instance Functor (I s) where
   fmap f (I m) = I $ \mv -> fmap go (m mv) where
     go (Pure a)        = Pure (f a)
     go (Fulfilled v k) = Fulfilled v (fmap (fmap f) k)
 
-instance Applicative I where
+instance Applicative (I s) where
   pure = return
   (<*>) = ap
 
-instance Monad I where
+instance Monad (I s) where
   return a = I $ \_ -> return $ Pure a
-  m >>= f = I $ \mv -> 
-    let go mv (Pure a) = runI (f a) mv
+  m >>= f = I $ \mv ->
+    let go mv (Pure a) = unI (f a) mv
         go mv (Fulfilled v k) = return $ Fulfilled v (k >>= go mv)
-    in runI m mv >>= go mv
+    in unI m mv >>= go mv
 
-runI :: I x -> x
-runI (I m) = unsafePerformIO $ do
-  mv <- newEmptyMVar
-  putMVar mv (m mv)
-  -- we need a better way to give something back!
-  
-
-newtype I a = I { runI :: forall x. MVar (IO (K x)) -> IO (K a) }
+runI :: (forall s. IVar s a -> I s ()) -> a
+runI f = unsafePerformIO $ do
+    mv <- newEmptyMVar
+    v <- newEmptyMVar
+    let iv = IVar mv v (drive mv v)
+    putMVar mv (unI (f iv) mv)
+    return $ demand iv
